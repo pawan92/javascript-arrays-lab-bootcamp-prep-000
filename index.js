1b9287785bac31e7d1ce42de6de97cd65faf1a67
Pawan M <pawanmurthy92@gmail.com>
2018-02-06 00:14:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,32 @@ function destructivelyAppendKitten(name){
   kittens.push(name);
 }
 
+function destructivelyPrependKitten(name) {
+   kittens.unshift(name);
+ }
+ 
+ function destructivelyRemoveLastKitten() {
+   kittens.pop();
+ }
+ 
+ function destructivelyRemoveFirstKitten() {
+   kittens.shift();
+ }
+ 
+ function appendKitten(name) {
+   return [...kittens, name];
+ }
+ 
+ function prependKitten(name) {
+   return [name, ...kittens];
+ }
+ 
+ function removeLastKitten() {
+   var newKits = kittens.slice(0, -1);
+   return newKits;
+ }
+ 
+ function removeFirstKitten() {
+   var newKits = kittens.slice(1);
+   return newKits;
+ }
